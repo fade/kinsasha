579a50553201b448e7c029ff0a5d8e8f1fe5e03f
Brian O'Reilly <fade@deepsky.com>
2009-04-03 18:28:24 -0400

    major renovations
    Ignore-this: ded6c9e545aace7a9d2afbccb642951e
    
    darcs-hash:20090403222824-8a24c-d15f6852f13cd7ea209b7c2c7d4b2b73772315a4.gz

--- kinsasha.lisp
+++ kinsasha.lisp
@@ -26,8 +26,8 @@
 
 (defun make-serve ()
   (ucw::make-backend
-   :httpd
-;;   :host "coruscant.deepsky.com"
+   :iolib
+   :host "0.0.0.0"
    :port 9999))
 
 (defclass knight-server (standard-server)
@@ -40,10 +40,11 @@
 
 (defvar *ktour-server* (make-ktour-server))
 
-(defclass ktour-application (basic-application)
+(defclass ktour-application (basic-application static-roots-application-mixin)
   ()
   (:default-initargs
-   :url-prefix "/"))
+   :url-prefix "/"
+    :www-roots (list (cons "" #P "/home/fade/SourceCode/lisp/kinsasha/"))))
 
 (defparameter *ktour-application*
   (make-instance 'ktour-application))
@@ -72,4 +73,45 @@
 (defmethod render ((self ktour-component))
   (<:H1 "Knight's Tour Solutions Incorporated!")
   (<:pre (format (html-stream (context.response *context*))
-		 (print-board (knights-tour)))))
+		  (print-board (knights-tour)))))
+
+(defentry-point "board.ucw" (:application *ktour-application*)
+  ()
+  (call 'ktour-display))
+
+(defcomponent ktour-display (standard-window-component)
+  ()
+  (:default-initargs
+      :body (make-instance 'ktour-component-board)))
+
+(defcomponent ktour-component-board ()
+  ())
+
+(defun giver (arr rank col &key colour)
+  (let ((whichone (aref arr rank col)))
+    (if colour
+	(format nil "images/~Ar.png" whichone)
+	(format nil "images/~Ab.png" whichone))))
+
+(defmethod render ((self ktour-component-board))
+  (<:H1 "Knight's Tour Solutions Inc.!")
+  (let* ((bboard (knights-tour)))
+    (ucw-core::remove-expired-sessions *ktour-application*)
+    (<:table :width 800
+	     (<:td (dotimes (i (array-dimension bboard 0) nil)
+		     (<:tr
+		      (dotimes (j (array-dimension bboard 1))
+			(if (= (mod (+ i j) 2) 0)
+			    (<:td (<:img :width 100 :src (giver bboard i j :colour t)))
+			    (<:td (<:img :width 100 :src (giver bboard i j)))
+			    ))))))))
+
+;; (defmethod render ((self ktour-component-board))
+;;   (<:H1 "Knight's Tour Solutions Inc.!")
+;;   (let* ((bboard (knights-tour)))
+;;     (<:td (dotimes (i (array-dimension bboard 0) nil)
+;; 	    (<:tr
+;; 	     (dotimes (j (array-dimension bboard 1))
+;; 	       (cond ((and (evenp i) (oddp j)) (<:td (<:img :src (giver bboard i j :colour t))))
+;; 		     ((and (oddp i) (evenp j)) (<:td (<:img :src (giver bboard i j)))))))))))
+
